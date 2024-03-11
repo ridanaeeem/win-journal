e0223e3bc21564f0bfa64f52a534ac59effedf6c
ridanaeeem <ridan@bu.edu>
2024-03-11 16:14:33 -0400

    update footer visibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Nav from "@/components/nav";
 import Logo from "@/components/logo";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -22,6 +23,7 @@ export default function RootLayout({
 					<Nav />
 				</div>
 				{children}
+				<Footer />
 				<Analytics />
 			</body>
 		</html>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,11 @@
 export default function Footer() {
 	return (
 		<nav className="w-full">
-			<ul className="flex justify-between p-[1rem]">
+			<ul className="flex justify-evenly p-[1rem] z-10 bg-[#ffffff]">
 				<a href="mailto: buwin@bu.edu">buwin@bu.edu</a>
-				<a href="https://www.instagram.com/bostonuwin/">Instagram</a>
+				<a href="https://www.instagram.com/bostonuwin/" target="_blank">
+					Instagram
+				</a>
 			</ul>
 		</nav>
 	);
