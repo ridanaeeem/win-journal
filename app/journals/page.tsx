25d6e637061b8ffc0b5f5dfb1e9436e825eb6d7e
ridanaeeem <ridan@bu.edu>
2024-04-03 12:10:30 -0400

    react-pageflip

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import JournalDisplay from "@/components/journal-display";
+import HTMLFlipBook from "react-pageflip";
+import testPage from "@/public/testPage.png";
+import Image from "next/image";
 
 export default function Journals() {
 	return (
@@ -7,8 +10,27 @@ export default function Journals() {
 			<title>Journals</title>
 			<main>
 				<h1 className="subtitle">All Journals</h1>
-				<div>
-					
+				<div className="book">
+					<HTMLFlipBook width={450} height={645} showCover={true} className="m-auto">
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+						<div className="demoPage bg-white text-black">
+							<Image src={testPage} alt="WIN logo" layout={"responsive"} />
+						</div>
+					</HTMLFlipBook>
 				</div>
 				<JournalDisplay />
 			</main>
