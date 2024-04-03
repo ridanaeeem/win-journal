a34b491b72d8c94edbf19a80c41ec8f5d0921785
ridanaeeem <ridan@bu.edu>
2024-04-03 10:39:06 -0400

    remove publuu

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -8,14 +8,7 @@ export default function Journals() {
 			<main>
 				<h1 className="subtitle">All Journals</h1>
 				<div>
-					<div className="w-full h-[75vh]">
-						<iframe
-							src="https://publuu.com/flip-book/430276/971857/page/1?embed&transparent"
-							width="100%"
-							height="100%"
-							scrolling="no"
-							allow="clipboard-write"></iframe>
-					</div>
+					
 				</div>
 				<JournalDisplay />
 			</main>
--- app/page.tsx
+++ app/page.tsx
@@ -7,31 +7,14 @@ export default function Home() {
 		<div>
 			<title>Home</title>
 			<main className="bg-gradient flex flex-col justify-between">
-				<div className="absolute right-[10vw] bottom-[10vh] h-1/2 ">
-					{/* <svg width="442" height="300" viewBox="0 0 442 577" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M33.9999 560L2.23042e-05 560L33.9999 536L33.9999 560Z" fill="#552C4C" />
-						<path d="M408 536L442 536L408 560L408 536Z" fill="#552C4C" />
-						<rect x="34" y="536" width="374" height="24" fill="#552C4C" />
-						<rect y="29" width="34" height="531" fill="#7B3C6D" />
-						<path d="M34 29L8.33394e-05 29L34 5L34 29Z" fill="#7B3C6D" />
-						<path
-							d="M33.9999 558.172L2.31611e-05 558.172L33.9999 535.828L33.9999 558.172Z"
-							fill="#F8F5F5"
-						/>
-						<path d="M408 535.828L442 535.828L408 558.172L408 535.828Z" fill="#F8F5F5" />
-						<rect x="34" y="535.828" width="374" height="22.3448" fill="#F8F5F5" />
-						<rect x="34" y="5" width="408" height="531" fill="#AB6E9E" />
-						<rect x="141" y="79" width="194" height="74" rx="30" fill="#E1A4D4" />
-					</svg> */}
-				</div>
-				<div className="flex w-full min-h-[50vh]">
+				<div className="flex w-full min-h-[40vh]">
 					<div>{/* <Image src={logo} alt="WIN logo" layout={"responsive"} /> */}</div>
 					<div className="p-24">
-						<h1 className="title">Undergraduate Journal of Neuroscience</h1>
+						<h1 className="title text-8xl">Undergraduate Journal of Neuroscience</h1>
 						<h2 className="py-2 text-xl">An initiative by Women In Neuroscience</h2>
 					</div>
 				</div>
-				<div className="w-full bg-white text-black p-24 text-xl min-h-[50vh] text-center text-2xl">
+				<div className="w-full bg-white text-black p-24 text-xl min-h-[40vh] text-center text-2xl">
 					<h3>
 						Our journal serves as a platform for showcasing neuroscience research conducted by Boston
 						University students.
