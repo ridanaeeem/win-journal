a66cbb69183b283ec994049e3a7160756ecd8cb0
ridanaeeem <ridan@bu.edu>
2024-04-04 00:29:48 -0400

    annotation fix

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -4,6 +4,7 @@ import HTMLFlipBook from "react-pageflip";
 import Image from "next/image";
 import { pdfjs, Document, Page } from "react-pdf";
 import { useState } from "react";
+import "react-pdf/dist/Page/AnnotationLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
