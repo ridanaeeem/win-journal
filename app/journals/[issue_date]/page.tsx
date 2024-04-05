4ea8666930f0e6c12bc105dc498bce065374458a
ridanaeeem <ridan@bu.edu>
2024-04-05 16:15:56 -0400

    issue number --> issue date for paths

--- app/journals/[issue_date]/page.tsx
+++ app/journals/[issue_date]/page.tsx
@@ -1,13 +1,13 @@
 import { Journal } from "@/types";
 import JournalDisplay from "@/components/journal-display";
 
-// the currently published issues - automate this
+// the dates of currently published issues - automate this
 export async function generateStaticParams() {
-	return [{ issue: "0" }, { issue: "1" }];
+	return [{ issue_date: "spring-2024" }, { issue_date: "fall-2025" }];
 }
 
-// gets the journal with the correct issue number
-async function getJournal(params: { issue: any }) {
+// gets the journal with the correct issue date
+async function getJournal(params: { issue_date: string }) {
 	// console.log("params");
 	// console.log(params);
 	const journals = await fetch("https://win-journal.vercel.app/api/journal").then((res) => res.json());
@@ -18,10 +18,10 @@ async function getJournal(params: { issue: any }) {
 	// console.log(journals.find((journal: Journal) => journal.issue === params.issue));
 	// console.log(params.issue);
 	// console.log(journals[Number(params.issue)]);
-	return journals[Number(params.issue)];
+	return journals.find((journal: Journal) => journal.issue_date === params.issue_date);
 }
-// displays the journal with the specified issue number of that page
-export default async function Journal({ params }: { params: { issue: any } }) {
+// displays the journal with the specified issue date of that page
+export default async function Journal({ params }: { params: { issue_date: any } }) {
 	const journal = await getJournal(params);
 
 	return (
