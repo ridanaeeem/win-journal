aca23b4aad0de582db374de6cb5283c076fe55c6
ridanaeeem <ridan@bu.edu>
2024-05-27 01:49:50 -0400

    journal submission form component

--- app/submissions/page.tsx
+++ app/submissions/page.tsx
@@ -1,10 +1,16 @@
+import React from "react";
+import JournalForm from "@/components/journal-form";
+
 export default function Submissions() {
 	return (
 		<div>
 			<title>Submissions</title>
 			<main>
-				<div>
-					<h1 className="subtitle">Submit an Article</h1>
+				<h1 className="subtitle">Submissions</h1>
+				<p></p>
+				{/* <div>
+					<h1 className="subtitle">Submissions</h1>
+					<h2></h2>
 					<form action="/api/submissions" method="post" className="flex flex-col text-xl m-10 pb-20">
 						<label htmlFor="title">Title</label>
 						<input type="text" id="title" name="title" required className="formfield" />
@@ -15,15 +21,16 @@ export default function Submissions() {
 						<label htmlFor="author_email">Google drive link</label>
 						<input type="text" id="author_email" name="author_email" required className="formfield"></input>
 						{/* <input type="file" id="article" name="article" accept="application/pdf"></input> */}
-						{/* <button type="submit">Submit</button> */}
-					</form>
-					{/* <iframe
+				{/* <button type="submit">Submit</button> */}
+				{/* </form> */}
+				{/* <iframe
 						src=""
 						width="640"
 						height="718">
 						Loading…
 					</iframe> */}
-				</div>
+
+				<JournalForm />
 			</main>
 		</div>
 	);
---
+++ components/journal-form.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+export default function JournalForm() {
+	return (
+		<div>
+			<h2>Journal application</h2>
+			<form action="" method="post" className="flex flex-col text-xl m-10 pb-20">
+				<div className="flex flex-col py-5">
+					<h3 className="text-4xl text-mainlight">Information about you</h3>
+					<label htmlFor="author">Full Name</label>
+					<input type="text" id="author" name="author" required className="formfield"></input>
+
+					<label htmlFor="email">BU Email</label>
+					<input type="text" id="email" name="email" required className="formfield"></input>
+
+					<label htmlFor="buID">BU ID</label>
+					<input type="text" id="buID" name="buID" required className="formfield"></input>
+
+					<label htmlFor="major">Major</label>
+					<input type="text" id="major" name="major" required className="formfield"></input>
+
+					<label htmlFor="gradYear">Expected Graduation Year</label>
+					<input type="text" id="gradYear" name="gradYear" required className="formfield"></input>
+				</div>
+
+				<div className="flex flex-col py-5">
+					<h3 className="text-4xl text-mainlight">Information about your research</h3>
+					<label htmlFor="lab">Where did you conduct your research?</label>
+					<input type="text" id="lab" name="lab" required className="formfield" />
+
+					<label htmlFor="labLink">Provide a link to your lab's website</label>
+					<input type="text" id="labLink" name="labLink" required className="formfield" />
+
+					<label htmlFor="labSummary">Provide a one sentence summary of what your lab specializes in.</label>
+					<input type="text" id="labSummary" name="labSummary" required className="formfield" />
+
+					<label htmlFor="length">
+						How long was your project, or how much time did you dedicate to your research?
+					</label>
+					<input type="text" id="length" name="length" required className="formfield" />
+				</div>
+
+				<div className="flex flex-col py-5">
+					<h3 className="text-4xl text-mainlight">Information about your paper</h3>
+					<label htmlFor="title">Article Title</label>
+					<input type="text" id="title" name="title" required className="formfield" />
+
+					<label htmlFor="subsection">
+						Which subsection is most appropriate for your journal article to be included in?
+					</label>
+					<div className="flex flex-col">
+						<div>
+							<input type="radio" id="neuro" name="subsection" value="Neuroscience" />
+							<label htmlFor="neuro" className="radio-input">
+								Neuroscience
+							</label>
+						</div>
+
+						<div>
+							<input type="radio" id="psych" name="subsection" value="Psychology" />
+							<label htmlFor="psych" className="radio-input">
+								Psychology
+							</label>
+						</div>
+
+						<div>
+							<input type="radio" id="comp" name="subsection" value="Computational Neuroscience" />
+							<label htmlFor="comp" className="radio-input">
+								Computational Neuroscience
+							</label>
+						</div>
+
+						<div>
+							<input type="radio" id="bio" name="subsection" value="General Biology" />
+							<label htmlFor="bio" className="radio-input">
+								General Biology
+							</label>
+						</div>
+					</div>
+
+					<label htmlFor="link">
+						Provide the Google drive link to your article.
+						<br></br>
+						*Make sure that anyone with the link can view!
+					</label>
+					<input type="text" id="link" name="link" required className="formfield"></input>
+
+					<label htmlFor="approval">
+						Provide the Google drive link to this form, signed by your Principal Investigator.
+					</label>
+					<input type="text" id="approval" name="approval" required className="formfield"></input>
+				</div>
+
+				{/* <button type="submit">Submit</button> */}
+			</form>
+		</div>
+	);
+}
