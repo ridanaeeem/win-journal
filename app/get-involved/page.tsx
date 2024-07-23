d96622e5c65e1075da0be542ae4fbf1baad832ad
ridanaeeem <ridan@bu.edu>
2024-07-23 09:40:01 -0500

    nav name function, get involved page

---
+++ app/get-involved/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Involved() {
+	return (
+		<div>
+			<title>Get Involved</title>
+			<main className="min-h-screen ">
+				<h1 className="subtitle">Get Involved</h1>
+				<div className="px-10 pb-10">
+					<p className="py-5 text-2xl"></p>
+				</div>
+			</main>
+		</div>
+	);
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -3,9 +3,23 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+function transformPathname(path: string) {
+	let result = path[1].toUpperCase();
+	for (let i = 2; i < path.length; i++) {
+		if (path[i] === "-" && i < path.length - 1) {
+			result += " " + path[i + 1].toUpperCase();
+			i++;
+		} else {
+			result += path[i];
+		}
+	}
+
+	return result;
+}
+
 export default function Nav() {
 	const pathname = usePathname();
-	const pages = ["/journals", "/authors", "/submissions", "/contact", "/about"];
+	const pages = ["/journals", "/authors", "/submissions", "/get-involved", "/contact", "/about"];
 	return (
 		<nav className="flex flex-row sticky top-0 bg-black z-10 text-white text-xl">
 			<div className={`p-[1rem] w-1/2 ${pathname === "/" ? "text-mainlight" : "text-white"}`}>
@@ -14,7 +28,7 @@ export default function Nav() {
 			<div className="flex justify-between p-[1rem] w-1/2">
 				{pages.map((page) => (
 					<Link href={page} key={page} className={`${pathname === page ? "text-mainlight" : "text-white"}`}>
-						{page.charAt(1).toUpperCase() + page.slice(2)}
+						{transformPathname(page)}
 					</Link>
 				))}
 			</div>
