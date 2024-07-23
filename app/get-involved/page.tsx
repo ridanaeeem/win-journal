4909cb8a96734399ae781531280a79abe56c9304
ridanaeeem <ridan@bu.edu>
2024-07-23 15:50:32 -0500

    submissions info expanded + collapsible component

--- app/get-involved/page.tsx
+++ app/get-involved/page.tsx
@@ -7,7 +7,15 @@ export default function Involved() {
 			<main className="min-h-screen ">
 				<h1 className="subtitle">Get Involved</h1>
 				<div className="px-10 pb-10">
-					<p className="py-5 text-2xl"></p>
+					<p className="py-5 text-2xl">
+						Thank you for taking interest in being apart of our team! This journal offers a variety of
+						roles, and we welcome anyone who is interested to apply to any position they seem fit.
+					</p>
+					<p className="py-5 text-2xl">
+						If interested in any of these positions, please email us directly at buwin@bu.edu with your full
+						name, year of graduation, and desired position. Thank you again for your interest in supporting
+						our community!
+					</p>
 				</div>
 			</main>
 		</div>
---
+++ components/collapsible.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Collapsible({ title, description }: { title: string; description: string }) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleCollapsible = () => {
+		setIsOpen((prev) => !prev);
+	};
+
+	return (
+		<div className="">
+			<button className="py-3 text-3xl text-mainlight" onClick={handleCollapsible}>
+				<div className="">
+					{title} <span className="text-lg text-mainlight">{isOpen ? "▲" : "▼"}</span>
+				</div>
+			</button>
+			<div className="text-2xl content">{isOpen ? <p>{description}</p> : <></>}</div>
+		</div>
+	);
+}
